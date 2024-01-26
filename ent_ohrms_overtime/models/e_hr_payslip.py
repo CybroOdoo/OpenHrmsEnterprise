f0b89a201afaf65cbdada1160d6ab21ea83374ae
Business Solutions For Africa <vincent@biz-4-africa.com>
2024-01-26 12:54:13 +0200

    Fix paysplip

--- ent_ohrms_overtime/models/e_hr_payslip.py
+++ ent_ohrms_overtime/models/e_hr_payslip.py
@@ -34,40 +34,41 @@ class PayslipOverTime(models.Model):
         input tree.
 
         """
-        input_data = []
-        res = super(PayslipOverTime, self)._compute_input_line_ids()
-        overtime_type = self.env.ref('ent_ohrms_overtime.hr_salary_rule_overtime')
-        overtime_input_type = self.env.ref('ent_ohrms_overtime.input_overtime_payroll')
-        contract = self.contract_id
-        overtime_id = self.env['hr.overtime'].search([('employee_id', '=', self.employee_id.id),
-                                                      ('contract_id', '=', self.contract_id.id),
-                                                      ('state', '=', 'approved'), ('payslip_paid', '=', False)])
-        hrs_amount = overtime_id.mapped('cash_hrs_amount')
-        day_amount = overtime_id.mapped('cash_day_amount')
-        cash_amount = sum(hrs_amount) + sum(day_amount)
-        old_input_rec = self.input_line_ids.filtered(lambda r: r.input_type_id.id == overtime_input_type.id)
-        if old_input_rec:
-            print(old_input_rec)
-            for rec in old_input_rec:
-                self.input_line_ids = [(2, rec.id, 0)]
-        if overtime_id and self.struct_id and overtime_input_type in self.struct_id.input_line_type_ids:
-            self.overtime_ids = overtime_id
-            input_data.append(Command.create({
-                'name': overtime_type.name,
-                'amount': cash_amount,
-                'input_type_id': overtime_input_type.id if overtime_input_type else 1
-            }))
-            # input_data = {
-            #     'name': overtime_type.name,
-            #     'code': overtime_type.code,
-            #     'amount': cash_amount,
-            #     'contract_id': contract.id,
-            #     'input_type_id': self.env.ref('ent_ohrms_overtime.input_overtime_payroll').id if self.env.
-            #     ref('ent_ohrms_overtime.input_overtime_payroll').id else 1
-            # }
-            # res.append(input_data)
-            print(input_data)
-            self.update({'input_line_ids': input_data})
+        for payslip in self:
+            input_data = []
+            res = super(PayslipOverTime, payslip)._compute_input_line_ids()
+            overtime_type = self.env.ref('ent_ohrms_overtime.hr_salary_rule_overtime')
+            overtime_input_type = self.env.ref('ent_ohrms_overtime.input_overtime_payroll')
+            contract = payslip.contract_id
+            overtime_id = self.env['hr.overtime'].search([('employee_id', '=', payslip.employee_id.id),
+                                                          ('contract_id', '=', payslip.contract_id.id),
+                                                          ('state', '=', 'approved'), ('payslip_paid', '=', False)])
+            hrs_amount = overtime_id.mapped('cash_hrs_amount')
+            day_amount = overtime_id.mapped('cash_day_amount')
+            cash_amount = sum(hrs_amount) + sum(day_amount)
+            old_input_rec = payslip.input_line_ids.filtered(lambda r: r.input_type_id.id == overtime_input_type.id)
+            if old_input_rec:
+                print(old_input_rec)
+                for rec in old_input_rec:
+                    self.input_line_ids = [(2, rec.id, 0)]
+            if overtime_id and payslip.struct_id and overtime_input_type in payslip.struct_id.input_line_type_ids:
+                payslip.overtime_ids = overtime_id
+                input_data.append(Command.create({
+                    'name': overtime_type.name,
+                    'amount': cash_amount,
+                    'input_type_id': overtime_input_type.id if overtime_input_type else 1
+                }))
+                input_data = {
+                    'name': overtime_type.name,
+                    'code': overtime_type.code,
+                    'amount': cash_amount,
+                    'contract_id': contract.id,
+                    'input_type_id': self.env.ref('ent_ohrms_overtime.input_overtime_payroll').id if self.env.
+                    ref('ent_ohrms_overtime.input_overtime_payroll').id else 1
+                }
+                res.append(input_data)
+                print(input_data)
+                payslip.update({'input_line_ids': input_data})
         return res
 
     def action_payslip_done(self):
